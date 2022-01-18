ba65521f1826498c16fba106ce94557640f833eb
Abdale Hassan <cabdale989@gmail.com>
2022-01-18 15:44:40 -0600

    Create a model for the user

---
+++ models/User.js
@@ -0,0 +1,64 @@
+const {Model, DataTypes} = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model{
+    checkPassword(loginPw){
+        return bcrypt.compareSync(loginPw,  this.password);
+    }
+}
+
+User.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true, 
+            },
+        },
+        password:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                len: [8],
+            },
+        },  
+    },
+
+{
+    hooks: {
+    beforeCreate: async(pass) =>{
+        pass.password = await bcrypt.hash(pass.password, 10);
+        return pass;
+        },
+    beforeUpdate: async (updatedPass) =>{
+        updatedPass.password = bcrypt.hash(updatedPass.password, 10)
+        return updatedPass;
+     },
+    },
+
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+},
+);
+
+module.exports = User;
