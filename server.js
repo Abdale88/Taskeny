4ff378c8711b7416ef42fdf8775c80b1adeae15c
Abdale Hassan <cabdale989@gmail.com>
2022-02-22 17:53:45 -0600

    set up handlebars as template and create custom helpers

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const path = require('path');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers')
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({helpers});
 
 const sess = {
     secret: 'Super secret secret',
@@ -20,6 +25,10 @@ const sess = {
 };
 
 app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine)
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
